ea78814db41d122b629e7d818fb875d0f06e7c16
ijinho <jinho9182@yonsei.ac.kr>
2019-12-12 12:40:14 +0900

    rewrite walltime.py

--- walltime.py
+++ walltime.py
@@ -7,6 +7,13 @@ command = "sbatch job_JH.sh"
 Popen(command, shell=True)
 
 h= WalltimeHandler(wall_time=36000,buffer_time=1000,electronic_step_stop=True)
+
+import time
+while True:
+    if h.check():
+        h.correct()
+    time.sleep(100)
+
 #h.check()
 #h.correct()
 
