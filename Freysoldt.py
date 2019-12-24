92165aa0cf762a71a07524686f1a7d128256f3eb
ijinho <jinho9182@yonsei.ac.kr>
2019-12-24 12:02:06 +0900

    delete test.py add 'Popen' and 'os.killpg' command in walltime.py create the 'Freysoldt.py' file.

--- Freysoldt.py
+++ Freysoldt.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-"""
-Model for the defect charge distribution
-q (r) = q [x exp(-r/gamma) + (1-x) exp(-r^2/beta^2)]
-"""
+
 
 q = float(input("charge value:  "))
 
 class PC(object):
-
+    """
+    Model for the defect charge distribution
+    q (r) = q [x exp(-r/gamma) + (1-x) exp(-r^2/beta^2)]
+    """
     def __init__(self):
         self.gamma = 1.0
         self.beta = 1.0
@@ -16,7 +16,10 @@ class PC(object):
         self.x = 0.0
 
     def q_model(self, g):
-
+        """
+        In reciprocal space, the defect charge distribution is chaged to (excluding
+        the Fourier normalization constant)
+        """
         return(self.x / np.sqrt(1+self.gamma * self.gamma * g*g)
                + (1 - self.x) * np.exp(-0.25*self.beta*self.beta*g*g))
 
--- walltime.py
+++ walltime.py
@@ -1,21 +1,22 @@
 #!/usr/local/bin/python3.6
 
 from custodian.vasp.handlers import WalltimeHandler
-from subprocess import *
+from subprocess import Popen
+import os
+import signal
+import time
 
-command = "sbatch job_JH.sh"
-Popen(command, shell=True)
+ppp = Popen(['mpirun -np 8 /home/jinho/programs/src/vasp.5.4.4_std/bin/vasp_std > stdout.log']
+      , shell=True, stdin=None, stdout=None, stderr=None, close_fds=True, preexec_fn=os.setpgrp)
 
 h= WalltimeHandler(wall_time=36000,buffer_time=1000,electronic_step_stop=True)
 
-import time
 while True:
     if h.check():
         h.correct()
     time.sleep(100)
 
-#h.check()
-#h.correct()
+os.killpg(os.getpgid(ppp.pid), signal.SIGTERM)
 
 exit()
 
