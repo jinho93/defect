0ecdb78eb4278ab2004e1d3c735a64ad4b455551
ijinho <jinho9182@yonsei.ac.kr>
2019-12-24 10:21:45 +0900

    Write PC part in kumagai_corre.py code

--- test.py
+++ test.py
@@ -1,4 +1,27 @@
-print('Hello')
+import numpy as np
 
-import numpy as  np
-print('SS')
+"""
+Model for the defect charge distribution
+q (r) = q [x exp(-r/gamma) + (1-x) exp(-r^2/beta^2)]
+"""
+
+q = float(input("charge value:  "))
+
+class PC(object):
+
+    def __init__(self):
+        self.gamma = 1.0
+        self.beta = 1.0
+        self.q = None
+        self.x = 0.0
+
+    def q_model(self, g):
+
+        return(self.x / np.sqrt(1+self.gamma * self.gamma * g*g)
+               + (1 - self.x) * np.exp(-0.25*self.beta*self.beta*g*g))
+
+
+
+
+
+
